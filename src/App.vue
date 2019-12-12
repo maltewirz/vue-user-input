7e4ca90df9fd87b5bdfb1c75b8052f3df9d91e5c
Malte Wirz <mail@maltewirz.com>
2019-12-12 09:45:13 +0100

    Input Forms done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container">
-        <form>
+        
+        <form v-if="!isSubmitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <h1>File a Complaint</h1>
@@ -11,7 +12,8 @@
                                 type="text"
                                 id="email"
                                 class="form-control"
-                                v-model="userData.email"
+                                :value="userData.email"
+                                @input="userData.email = $event.target.value"
                                 >
                     </div>
                     <div class="form-group">
@@ -110,14 +112,23 @@
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <Switcher
+                        v-model="dataSwitch"
+                    ></Switcher>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                            @click.prevent="submitted"
+                            >Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -133,16 +144,19 @@
                             <li v-for="(item,index) in sendMail" :key="index">{{item}}</li>
                         </ul>
                         <p>Gender: {{ gender }}</p>
-                        <p>Priority:</p>
-                        <p>Switched:</p>
+                        <p>Priority: {{ selectedPriority }}</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
         </div>
+        
     </div>
 </template>
 
 <script>
+    import Switcher from './Switcher.vue';
+
     export default {
         data () {
             return {
@@ -155,8 +169,18 @@
                 sendMail: [],
                 gender: 'Male',
                 priorities: ['High', 'Medium', 'Low'],
-                selectedPriority: 'High'
+                selectedPriority: 'High',
+                dataSwitch: true,
+                isSubmitted: false
             }
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true
+            }
+        },
+        components: {
+            Switcher,
         }
     }
 </script>
